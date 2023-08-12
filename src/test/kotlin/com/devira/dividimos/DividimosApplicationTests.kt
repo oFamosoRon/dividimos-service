0cb031f5edfa11d7c1d5f13cf08f3fb0fcb1dcfd
oFamosoRon <ofamosoron@gmail.com>
2023-08-12 11:39:30 -0300

    First commit

---
+++ src/test/kotlin/com/devira/dividimos/DividimosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devira.dividimos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DividimosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
